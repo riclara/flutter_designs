422b2bef956ad7af2d924e5db389cfc46e421044
Ricardo Lara <riclara@gmail.com>
2020-01-23 22:19:56 -0500

    add actions and text box

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -10,31 +10,72 @@ class BasicPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Image(
-            image: NetworkImage('https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
-          ),
-
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Lago con montañas', style: titleStyle,),
-                      SizedBox(height: 7.0,),
-                      Text('Lago con montañas que no se donde', style: subTitleStyle,)
-                    ],
-                  ),
-                ),
-                Icon( Icons.star, color: Colors.red, size: 30.0,),
-                Text('42', style: TextStyle(fontSize: 20.0),)
-              ],
-            ),
-          )
+          _buildImage(),
+          _buildTitle(),
+          _buildActions(),
+          _buildTextBox()
         ],
       )
     );
   }
+
+  Widget _buildImage () {
+    return Image(
+      image: NetworkImage('https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+    );
+  }
+
+  Widget _buildTitle () {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Lago con montañas', style: titleStyle,),
+                SizedBox(height: 7.0,),
+                Text('Lago con montañas que no se donde', style: subTitleStyle,)
+              ],
+            ),
+          ),
+          Icon( Icons.star, color: Colors.red, size: 30.0,),
+          Text('42', style: TextStyle(fontSize: 20.0),)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActions() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _action('Call', Icons.call),
+        _action('Route', Icons.near_me),
+        _action('Share', Icons.share),
+      ],
+    );
+  }
+
+  Widget _buildTextBox () {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Magna voluptate anim aliquip proident nisi cillum quis voluptate nulla. Ea amet cillum labore duis. Anim amet tempor duis mollit sunt voluptate officia consectetur laborum aute consequat. Consectetur laborum nisi aliquip tempor anim. Esse ipsum ea Lorem enim esse consequat nulla Lorem quis.',
+        textAlign: TextAlign.justify
+      )
+    );
+  }
+
+  Widget _action(String txt, IconData ico) {
+    return Column(
+      children: <Widget>[
+        Icon(ico, color: Colors.blue, size: 40.0,),
+        SizedBox(height: 5.0,),
+        Text(txt, style: TextStyle(fontSize: 15.0, color: Colors.blue),)
+      ],
+    );
+  }
+
 }
